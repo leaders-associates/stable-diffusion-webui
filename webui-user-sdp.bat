116ee56b86b570ceaa11810c71fbeecf6c45f93b
Ryan <yonguk1215@gmail.com>
2023-10-31 21:57:45 +0900

    webui_160

---
+++ webui-user-sdp.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS= --opt-sdp-attention --opt-sdp-no-mem-attention --theme dark --no-half-vae --autolaunch --medvram
+
+call webui.bat
